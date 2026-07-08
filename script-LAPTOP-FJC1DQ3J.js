744e242dfff534884e109e68d747aea30b2ed18a
Subash B <thapasubash9072@gmail.com>
2026-07-08 14:34:05 +0530

    adding all

---
+++ script-LAPTOP-FJC1DQ3J.js
@@ -0,0 +1,19 @@
+// // Smooth scroll function for navigation
+// function scrollToSection(sectionId) {
+//     const section = document.getElementById(sectionId);
+//     window.scrollTo({
+//         top: section.offsetTop - 80, // Adjust for fixed header
+//         behavior: 'smooth'
+//     });
+// }
+
+// // Contact form submission (for demo purposes, no actual server logic)
+// document.getElementById('contactForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     alert('Thank you for your message!');
+//     document.getElementById('contactForm').reset();
+// });
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector("#main"),
+//     smooth: true
+// });
